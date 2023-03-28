9f934cabcc2fe0ef78715e0133fb4ab1fe0959b0
lpubsppop01 <lpubsppop01@gmail.com>
2023-03-28 14:55:12 +0000

    Update color change panel layout

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dimensions {
+  static const double fontSizeNormal = 18;
+  static const double heightNaviBar = 50;
+  static const double thicknessDivider = 1;
+  static const double marginBodyLeft = 10;
+  static const double marginBodyRight = 10;
+  static const EdgeInsets marginBodyLTR = EdgeInsets.fromLTRB(10, 10, 10, 0);
+  static const double marginNaviLeft = 10;
+  static const double marginNaviRight = 10;
+  static const double marginNaviTitleLeft = 72;
+  static const double spacingButtons = 10;
+  static const double spacingRadios = 10;
+}
--- lib/widgets/change_color_panel.dart
+++ lib/widgets/change_color_panel.dart
@@ -1,3 +1,4 @@
+import 'package:ccosvg/constants.dart';
 import 'package:ccosvg/models/svg_color_change.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -32,31 +33,100 @@ class _ChangeColorPanelState extends State<ChangeColorPanel> {
       child: Column(children: [
         SizedBox(
             width: double.infinity,
-            child: Container(
-                margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-                child: Text("Delta of ${_getLongLabel()}",
-                    style: const TextStyle(
-                      fontSize: 18,
-                    )))),
-        Column(children: [
-          RadioListTile(
-            title: const Text("Value"),
-            value: SvgColorChangeDeltaKind.value,
-            groupValue: _deltaKind,
-            onChanged: deltaKindRadioChanged,
-          ),
-          RadioListTile(
-            title: const Text("Percentage in Limit"),
-            value: SvgColorChangeDeltaKind.percentageInLimit,
-            groupValue: _deltaKind,
-            onChanged: deltaKindRadioChanged,
-          ),
-        ]),
+            height: Dimensions.heightNaviBar,
+            child: Stack(children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: const EdgeInsets.only(left: Dimensions.marginNaviTitleLeft),
+                  child: Text("Delta of ${_getLongLabel()}", style: const TextStyle(fontSize: Dimensions.fontSizeNormal)),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: const EdgeInsets.only(left: Dimensions.marginNaviLeft),
+                  child: IconButton(
+                    onPressed: () => widget.onCancelled?.call(),
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  margin: const EdgeInsets.only(right: Dimensions.marginNaviRight),
+                  child: IconButton(
+                    onPressed: () => widget.onDecided?.call(_buildChange()),
+                    icon: const Icon(Icons.check),
+                  ),
+                ),
+              ),
+            ])),
+        Divider(
+          color: Theme.of(context).dividerColor,
+          thickness: Dimensions.thicknessDivider,
+          height: Dimensions.thicknessDivider,
+        ),
+        Container(
+          margin: Dimensions.marginBodyLTR,
+          child: Row(children: [
+            InkWell(
+              child: Row(children: [
+                Radio(
+                  value: SvgColorChangeDeltaKind.value,
+                  groupValue: _deltaKind,
+                  onChanged: deltaKindRadioChanged,
+                ),
+                const Text(
+                  "Value",
+                  style: TextStyle(fontSize: Dimensions.fontSizeNormal),
+                ),
+              ]),
+              onTap: () {
+                setState(() {
+                  deltaKindRadioChanged(SvgColorChangeDeltaKind.value);
+                });
+              },
+            ),
+            const SizedBox(width: Dimensions.spacingRadios),
+            InkWell(
+              child: Row(children: [
+                Radio(
+                  value: SvgColorChangeDeltaKind.percentageInLimit,
+                  groupValue: _deltaKind,
+                  onChanged: deltaKindRadioChanged,
+                ),
+                const Text(
+                  "Percentage in Limit",
+                  style: TextStyle(fontSize: Dimensions.fontSizeNormal),
+                ),
+              ]),
+              onTap: () {
+                deltaKindRadioChanged(SvgColorChangeDeltaKind.percentageInLimit);
+              },
+            ),
+          ]),
+        ),
+        Container(
+            margin: Dimensions.marginBodyLTR,
+            child: TextField(
+              controller: _textEditController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: const TextInputType.numberWithOptions(signed: true),
+              textAlign: TextAlign.right,
+              inputFormatters: [
+                TextInputFormatter.withFunction(
+                    (oldValue, newValue) => RegExp(r"^-?[0-9]*$").hasMatch(newValue.text) ? newValue : oldValue)
+              ],
+            )),
         Visibility(
           child: SizedBox(
             width: double.infinity,
             child: Container(
-              margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+              margin: Dimensions.marginBodyLTR,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
@@ -64,17 +134,17 @@ class _ChangeColorPanelState extends State<ChangeColorPanel> {
                     child: const Text("-10"),
                     onPressed: () => addValueButtonPressed(-10),
                   ),
-                  const SizedBox(width: 8),
+                  const SizedBox(width: Dimensions.spacingButtons),
                   ElevatedButton(
                     child: const Text("-1"),
                     onPressed: () => addValueButtonPressed(-1),
                   ),
-                  const SizedBox(width: 8),
+                  const SizedBox(width: Dimensions.spacingButtons),
                   ElevatedButton(
                     child: const Text("+1"),
                     onPressed: () => addValueButtonPressed(1),
                   ),
-                  const SizedBox(width: 8),
+                  const SizedBox(width: Dimensions.spacingButtons),
                   ElevatedButton(
                     child: const Text("+10"),
                     onPressed: () => addValueButtonPressed(10),
@@ -89,7 +159,7 @@ class _ChangeColorPanelState extends State<ChangeColorPanel> {
           child: SizedBox(
             width: double.infinity,
             child: Container(
-              margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+              margin: Dimensions.marginBodyLTR,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
@@ -97,17 +167,17 @@ class _ChangeColorPanelState extends State<ChangeColorPanel> {
                     child: const Text("-25"),
                     onPressed: () => addValueButtonPressed(-25),
                   ),
-                  const SizedBox(width: 8),
+                  const SizedBox(width: Dimensions.spacingButtons),
                   ElevatedButton(
                     child: const Text("-10"),
                     onPressed: () => addValueButtonPressed(-10),
                   ),
-                  const SizedBox(width: 8),
+                  const SizedBox(width: Dimensions.spacingButtons),
                   ElevatedButton(
                     child: const Text("+10"),
                     onPressed: () => addValueButtonPressed(10),
                   ),
-                  const SizedBox(width: 8),
+                  const SizedBox(width: Dimensions.spacingButtons),
                   ElevatedButton(
                     child: const Text("+25"),
                     onPressed: () => addValueButtonPressed(25),
@@ -117,39 +187,6 @@ class _ChangeColorPanelState extends State<ChangeColorPanel> {
             ),
           ),
           visible: _deltaKind == SvgColorChangeDeltaKind.percentageInLimit,
-        ),
-        Container(
-            margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-            child: TextField(
-              controller: _textEditController,
-              keyboardType: const TextInputType.numberWithOptions(signed: true),
-              textAlign: TextAlign.right,
-              inputFormatters: [
-                TextInputFormatter.withFunction(
-                    (oldValue, newValue) => RegExp(r"^-?[0-9]*$").hasMatch(newValue.text) ? newValue : oldValue)
-              ],
-            )),
-        SizedBox(
-          width: double.infinity,
-          child: Container(
-            margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  child: const Text("Cancel"),
-                  onPressed: () => widget.onCancelled?.call(),
-                ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  child: const Text("OK"),
-                  onPressed: () {
-                    widget.onDecided?.call(_buildChange());
-                  },
-                ),
-              ],
-            ),
-          ),
         ),
       ]),
     );
