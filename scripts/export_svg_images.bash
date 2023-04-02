30f29eb337af66d63ed2ba8397b6a1b536a8a2af
lpubsppop01 <lpubsppop01@gmail.com>
2023-04-02 12:13:56 +0000

    Add maskable app icon SVG file and fix some issues

--- scripts/export_svg_images.bash
+++ scripts/export_svg_images.bash
@@ -20,12 +20,12 @@ export_ui_icons() {
 }
 
 export_app_icon() {
-  SVG_PATH='images/app_icon.svg'
-
-  $INKSCAPE $SVG_PATH --export-filename 'assets/app_icon.png' -w 24 -h 24
-  $INKSCAPE $SVG_PATH --export-filename 'web/favicon.png' -w 16 -h 16
-  $INKSCAPE $SVG_PATH --export-filename 'web/icons/Icon-192.png' -w 192 -h 192
-  $INKSCAPE $SVG_PATH --export-filename 'web/icons/Icon-512.png' -w 512 -h 512
+  $INKSCAPE 'images/app_icon.svg' --export-filename 'web/favicon.png' -w 16 -h 16
+  $INKSCAPE 'images/app_icon.svg' --export-filename 'web/icons/Icon-44.png' -w 44 -h 44
+  $INKSCAPE 'images/app_icon_maskable.svg' --export-filename 'web/icons/Icon-192.png' -w 192 -h 192
+  $INKSCAPE 'images/app_icon_maskable.svg' --export-filename 'web/icons/Icon-512.png' -w 512 -h 512
+  $INKSCAPE 'images/app_icon_maskable.svg' --export-filename 'web/icons/Icon-maskable-192.png' -w 192 -h 192
+  $INKSCAPE 'images/app_icon_maskable.svg' --export-filename 'web/icons/Icon-maskable-512.png' -w 512 -h 512
 }
 
 export_ui_icons
